f72a1e53553b228719b39e6517544d6c868e79da
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-08-14 23:12:00 +0800

    Added state management

--- src/main.js
+++ src/main.js
@@ -4,11 +4,18 @@ import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 import router from './router'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   router,
+  pinia,
   render: h => h(App)
 }).$mount('#app')
