346eb2b89bf3eab16a29b5a75d1619fd294d8848
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-08-18 23:05:25 +0800

    Colors!

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,28 +1,29 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-import id from 'vuetify/lib/locale/id';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import id from "vuetify/lib/locale/id";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
-      options: {
-        customProperties: true,
-      },
+    options: {
+      customProperties: true,
+    },
     themes: {
       light: {
-        primary: '#007BFF',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
+        primary: colors.blue.darken4,
+        secondary: "#424242",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
       },
     },
   },
-    lang: {
-      locales: { id },
-      current: 'id',
-    },
+  lang: {
+    locales: { id },
+    current: "id",
+  },
 });
