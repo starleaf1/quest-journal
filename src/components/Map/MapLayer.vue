21c86693ea91841eb855bd9e90af3bf614021f1c
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-08-20 16:24:45 +0800

    Better API key

---
+++ src/components/Map/MapLayer.vue
@@ -0,0 +1,22 @@
+<template>
+  <l-map ref="primaryMap" style="height: 100%; z-index: 0;" :zoom="11" :center="center">
+    <l-tile-layer :url="tileUrl" />
+  </l-map>
+</template>
+
+<script>
+export default {
+  name: 'MapLayer',
+  data () {
+    return ({
+      tileUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      center: [-8.4537137,114.5110415]
+    })
+  },
+  mounted () {
+    this.$nextTick().then(() => {
+      this.$refs.primaryMap.mapObject.invalidateSize()
+    })
+  }
+}
+</script>
