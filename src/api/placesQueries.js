ccd82ea64be383630d4c8a6aa444ec14998afd87
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-08-26 18:09:38 +0800

    Adding place detail query

--- src/api/placesQueries.js
+++ src/api/placesQueries.js
@@ -2,6 +2,26 @@ import GeoPoint from "geopoint";
 
 const google = window.google;
 
+const service = new google.maps.places.PlacesService(
+  document.createElement("div")
+);
+
+export const getPlaceDetails = (
+  placeId,
+  fields = [
+    "formatted_address",
+    "business_status",
+    "formatted_phone_number",
+    "opening_hours",
+  ]
+) =>
+  new Promise((resolve, reject) => {
+    service.getDetails({ placeId, fields }, (result, status) => {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) reject(result);
+      resolve(result);
+    });
+  });
+
 export const getPlacesInBounds = (bounds) =>
   new Promise((resolve, reject) => {
     const googleFormattedBounds = new google.maps.LatLngBounds(
@@ -16,9 +36,6 @@ export const getPlacesInBounds = (bounds) =>
 
     const radius = origin.distanceTo(destination) * 1000;
 
-    const service = new google.maps.places.PlacesService(
-      document.createElement("div")
-    );
     service.nearbySearch(
       { location: searchCenter, radius },
       (results, status) => {
