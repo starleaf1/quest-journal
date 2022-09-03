9b49d2d5ebc6d1343dc48850c9e93397e7de903a
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-09-03 12:29:46 +0800

    New filtering technique

--- src/components/Map/MarkerGroup.vue
+++ src/components/Map/MarkerGroup.vue
@@ -55,22 +55,23 @@ export default {
         }
 
         const placeFilter = newPlace => {
-          const isUnique = existing.some(existingPlace => existingPlace.place_id !== newPlace.place_id)
+          const isUnique = !existing.some(existingPlace => existingPlace.place_id === newPlace.place_id)
           console.debug('[place-filter] Checking if place exists', newPlace.place_id, isUnique)
           return isUnique
         }
 
         const existing = _.cloneDeep(this.$data.places)
-        if (existing.length >= 200) {
-          existing.splice(0, newPlaces.length)
-        }
 
         const unique = newPlaces.filter(placeFilter)
-        this.$data.places = [...existing, ...unique]
+        const newCollection = [...existing, ...unique]
+        if (newCollection.length > 200) newCollection.splice(0, newCollection.length - 200)
+        this.$data.places = newCollection
       }
 
       const fetchPlaces = async () => {
+        console.debug('[marker-group] Getting place list')
         const places = await getPlacesInBounds(e);
+        console.debug('[marker-group] Fetch complete', places)
         appendPlaces(places)
       }
       return _.debounce(fetchPlaces, 1000, { leading: true, trailing: false })()
