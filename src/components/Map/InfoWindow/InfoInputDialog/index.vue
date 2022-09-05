da86717fd7eaa95c9f6e37dc50d75274dfc710b7
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-09-05 17:09:48 +0800

    Added the addition dialog

---
+++ src/components/Map/InfoWindow/InfoInputDialog/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog v-model="dialogOpen">
+    <template #activator="{ on, attrs }">
+      <slot name="activator" v-on="on" v-bind="attrs" />
+    </template>
+    <v-sheet>
+      <v-toolbar>
+        <v-btn icon>
+          <v-icon>mdi-cancel</v-icon>
+        </v-btn>
+        <v-toolbar-title>Save to your collection</v-toolbar-title>
+      </v-toolbar>
+      <v-form>
+
+      </v-form>
+    </v-sheet>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'InfoInputDialog',
+  data () {
+    return ({
+      dialogOpen: false
+    })
+  },
+  methods: {
+    closeDialog () {
+      this.$data.dialogOpen = false
+    }
+  }
+}
+</script>
