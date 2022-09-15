4e7cd7b8b782cc9c2268cf937f7cb54c3053db13
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-09-16 00:10:42 +0800

    Added serverTimestamp for registered users

--- functions/auth-hooks/index.js
+++ functions/auth-hooks/index.js
@@ -1,9 +1,16 @@
 const functions = require('firebase-functions')
 const { getFirestore } = require('firebase-admin/firestore')
+const { serverTimestamp } = require('firebase/firestore')
 
 const firestore = getFirestore()
-const onUserSignUp = functions.auth.user().onCreate(user => {
-  firestore.doc(`userData/${user.uid}`).set({ greeting: 'Hello, World!' })
+const onUserSignUp = functions.auth.user().onCreate(async user => {
+  try {
+    await Promise.all([
+      firestore.doc(`userData/${user.uid}`).set({ registered: serverTimestamp() })
+    ])
+  } catch (e) {
+    console.error('[on-user-signup-trigger]', e)
+  }
 })
 
 module.exports = {
