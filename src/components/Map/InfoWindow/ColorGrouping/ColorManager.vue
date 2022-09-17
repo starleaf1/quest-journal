24dfb780d9d711a835068a1a8999928aa6a18681
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-09-17 20:47:33 +0800

    Added color manager

--- src/components/Map/InfoWindow/ColorGrouping/ColorManager.vue
+++ src/components/Map/InfoWindow/ColorGrouping/ColorManager.vue
@@ -1,14 +1,28 @@
 <template>
-  
+  <v-card>
+    <v-card-title>Manage Color &amp; Categories</v-card-title>
+    <v-card-text>
+      <v-data-table
+        :items="categories"
+      >
+        <template #[`item.color`]="{ item }">
+          <v-chip :color="item.color">&nbsp;</v-chip>
+        </template>
+      </v-data-table>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+import { useCategoriesStore } from '@/store/categoriesStore'
+import { mapActions } from 'pinia'
 export default {
-  name: 'ColorInput',
-  data () {
-    return {
-      internalColor: ''
-    }
+  name: 'ColorManager',
+  computed: {
+    ...mapState(useCategoriesStore, ['categories']),
+  },
+  methods: {
+    ...mapActions(useCategoriesStore, ['modify'])
   }
 }
 </script>
