6e765d813c389014b90fea49a8d4b86b5d40efdd
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-09-20 15:50:32 +0800

    Created the custom component

---
+++ src/components/Map/SavedPlacesLayer/ColoredMarker.vue
@@ -0,0 +1,25 @@
+<template>
+  <l-marker
+    :lat-lng="latLng"
+  />
+</template>
+
+<script>
+import L from "leaflet"
+
+export default {
+  name: 'ColoredMarker',
+  props: {
+    color: String
+  },
+  computed: {
+    icon () {
+      return L.icon({
+        iconUrl: require(`@/assets/markers/${this.color}.svg`),
+        iconSize: [24, 24],
+        iconAnchor: [12, 24]
+      })
+    }
+  }
+}
+</script>
---
+++ src/components/Map/SavedPlacesLayer/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <l-layer-group></l-layer-group>
+</template>
+
+<script>
+import { mapState } from 'pinia'
+import { useSavedPlacesStore } from '@/store/savedPlaces'
+
+export default {
+  name: 'SavedPlacesMarker',
+  computed: {
+    ...mapState(useSavedPlacesStore, ['savedPlaces'])
+  },
+  methods: {
+    handleMarkerClick () {
+      
+    }
+  }
+}
+</script>
