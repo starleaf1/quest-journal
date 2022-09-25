96552ac802d7f18d813abfcff7b0fdb582999dde
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-09-25 23:26:16 +0800

    Added dialog to see saved places

--- src/components/PlacesList/PlaceItem.vue
+++ src/components/PlacesList/PlaceItem.vue
@@ -1,40 +1,48 @@
 <template>
-  <v-list-item
-    three-line
-  >
-    <v-list-item-content>
-      <v-list-item-title>{{place.name}}</v-list-item-title>
-      <v-list-item-subtitle>{{place.formatted_address}}</v-list-item-subtitle>
-    </v-list-item-content>
-    <v-list-item-action>
-      <v-btn
-        icon
-        color="primary"
-        @click="handleMapSearchClick(place)"
+  <v-dialog>
+    <template #activator="{ on }">
+      <v-list-item
+        three-line
+        v-on="on"
       >
-        <v-icon>mdi-map-search</v-icon>
-      </v-btn>
-    </v-list-item-action>
-  </v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>{{place.name}}</v-list-item-title>
+          <v-list-item-subtitle>{{place.formatted_address}}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn
+            icon
+            color="primary"
+            @click.stop="handleMapSearchClick(place)"
+          >
+            <v-icon>mdi-map-search</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </template>
+    <SavedInfoDialog :place="place" />
+  </v-dialog>
 </template>
 
 <script>
 import { mapActions } from 'pinia'
 import { useComponentCommunicator } from '@/store/componentCommunicator'
+import SavedInfoDialog from './SavedInfoDialog.vue'
 export default {
-  name: 'PlaceItem',
-  props: {
-    place: {
-      type: Object,
-      required: true
+    name: "PlaceItem",
+    props: {
+        place: {
+            type: Object,
+            required: true
+        },
     },
-  },
-  methods: {
-    ...mapActions(useComponentCommunicator, ['orderMapPan', 'markPlace']),
-    handleMapSearchClick (place) {
-      this.orderMapPan({ ...place.geometry.location, zoom: 17 })
-      this.markPlace({ ...place })
-    }
-  }
+    methods: {
+        ...mapActions(useComponentCommunicator, ["orderMapPan", "markPlace"]),
+        handleMapSearchClick(place) {
+            this.orderMapPan({ ...place.geometry.location, zoom: 17 });
+            this.markPlace({ ...place });
+        }
+    },
+    components: { SavedInfoDialog }
 }
 </script>
---
+++ src/components/PlacesList/SavedInfoDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-card-title>{{place.name}}</v-card-title>
+    <v-card-text>
+      <p>{{place.notes}}</p>
+      <v-chip-group>
+        <v-chip
+          outlined
+          v-for="tag in place.tags"
+          :key="tag"
+        />
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'SavedInfoDialog',
+  props: {
+    place: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
