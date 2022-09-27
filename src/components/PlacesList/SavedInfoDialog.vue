4c2681e4816b1ac10842dbb15d358ef7ceecd61a
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-09-27 22:04:47 +0800

    Added delete method

--- src/components/PlacesList/SavedInfoDialog.vue
+++ src/components/PlacesList/SavedInfoDialog.vue
@@ -8,19 +8,39 @@
           outlined
           v-for="tag in place.tags"
           :key="tag"
-        />
+        >
+          {{ tag }}
+        </v-chip>
       </v-chip-group>
     </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+        color="error"
+        text
+        @click="handleDeleteButtonClick"
+      >
+        Delete
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import { mapActions } from 'pinia'
+import { useSavedPlacesStore } from '@/store/savedPlaces'
 export default {
   name: 'SavedInfoDialog',
   props: {
     place: {
       required: true,
       type: Object
+    }
+  },
+  methods: {
+    ...mapActions(useSavedPlacesStore, ['remove']),
+    async handleDeleteButtonClick () {
+      await this.remove(this.place.place_id)
     }
   }
 }
