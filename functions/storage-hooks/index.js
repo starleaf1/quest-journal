bcf73b7c56ca3f2261e0b838bdd1108e8836dda7
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-10-12 16:55:12 +0800

    Fixed wrong import for getFirestore

--- functions/storage-hooks/index.js
+++ functions/storage-hooks/index.js
@@ -1,22 +1,20 @@
-const { getFirestore } = require('firebase-admin')
-const functions = require('firebase-functions')
-const { serverTimestamp } = require('firebase/firestore')
+const { getFirestore } = require("firebase-admin/firestore");
+const functions = require("firebase-functions");
 
-const registerPhotoInFirestore = functions.storage.object().onFinalize(async object => {
-  if (!object.name.match(/^uploads\/.*\/images\/places\/.*/)) return
-  if (!object.contentType.startsWith('image/')) throw new Error('Uploaded object is not image')
-  const { name } = object
-  const [, uid, , , placeId, ] = name.split('/')
+const registerPhotoInFirestore = functions.storage
+  .object()
+  .onFinalize(async (object) => {
+    if (!object.name.match(/^uploads\/.*\/images\/places\/.*/)) return;
+    if (!object.contentType.startsWith("image/"))
+      throw new Error("Uploaded object is not image");
+    const { name } = object;
+    const [, uid, , , placeId] = name.split("/");
 
-  const firestore = getFirestore()
-  try {
-    await firestore.doc(`userData/${uid}/places/${placeId}/images`).set({
-      uploaded: serverTimestamp(),
-      gcsPath: name
-    })
-  } catch (e) {
-    console.error('Failed to register in Firestore')
-  }
-})
+    const firestore = getFirestore();
+    await firestore.collection(`userData/${uid}/places/${placeId}/images`).add({
+      uploaded: new Date(),
+      gcsPath: name,
+    });
+  });
 
-module.exports = { registerPhotoInFirestore }
+module.exports = { registerPhotoInFirestore };
