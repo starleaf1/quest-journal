77992065c4089a94369720d413374b30b99d9842
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-11-09 16:45:54 +0800

    Added placeId stuff.

--- src/store/savePlaceDialogStore.js
+++ src/store/savePlaceDialogStore.js
@@ -1,21 +1,25 @@
-import { defineStore } from "pinia"
-import { ref } from "vue"
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useSavePlaceDialogStore = defineStore('savePlaceDialogStore', () => {
-  const isSaveDialogOpen = ref(false)
+export const useSavePlaceDialogStore = defineStore(
+  "savePlaceDialogStore",
+  () => {
+    const isSaveDialogOpen = ref(false);
+    const placeId = ref("");
 
-  const openSaveDialog = () => {
-    isSaveDialogOpen.value = true
-  }
-  const closeSaveDialog = () => {
-    isSaveDialogOpen.value = false
-  }
-  const placeId = ref('')
+    const openSaveDialog = (id) => {
+      isSaveDialogOpen.value = true;
+      placeId.value = id;
+    };
+    const closeSaveDialog = () => {
+      isSaveDialogOpen.value = false;
+    };
 
-  return {
-    isSaveDialogOpen,
-    openSaveDialog,
-    closeSaveDialog,
-    placeId
+    return {
+      isSaveDialogOpen,
+      openSaveDialog,
+      closeSaveDialog,
+      placeId,
+    };
   }
-})
+);
