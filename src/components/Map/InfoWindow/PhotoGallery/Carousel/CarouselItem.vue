76cebedb95ab6e01a5c000813f2fac7500d42487
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-12-21 23:29:49 +0800

    Separated the images into their own component.

---
+++ src/components/Map/InfoWindow/PhotoGallery/Carousel/CarouselItem.vue
@@ -0,0 +1,25 @@
+<script>
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    image: {
+      required: true,
+      type: Object
+    }
+  },
+  setup(props) {
+    const src = computed(() => props.image.getUrl())
+    return { src }
+  },
+})
+</script>
+
+<template>
+  <v-img
+    :src="src"
+    width="100vw"
+    height="100vh"
+    contain
+  />
+</template>
