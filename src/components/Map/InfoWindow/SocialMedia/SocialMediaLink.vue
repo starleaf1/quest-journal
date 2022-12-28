a41e6b700f5bd7352cccad8a187e34afb9756fd5
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-12-29 00:00:23 +0800

    Pointed to correct assets and colors.

--- src/components/Map/InfoWindow/SocialMedia/SocialMediaLink.vue
+++ src/components/Map/InfoWindow/SocialMedia/SocialMediaLink.vue
@@ -42,12 +42,32 @@ export default defineComponent({
           return '#4267B2'
         case 'youtube':
           return '#f00'
+        case 'booking.com':
+          return '#003b95'
+        case 'traveloka':
+          return '#1ba0e2'
+        case 'agoda':
+        case 'tripadvisor':
+        case 'tiket.com':
+          return '#fff'
         default:
           return 'primary'
       }
     })
 
-    return { icon, color }
+    const bigLogoSrc = computed(() => {
+      const logos = [
+        { name: "booking.com", src: require("@/assets/logo/booking_com_blue.png") },
+        { name: "traveloka", src: require("@/assets/logo/traveloka-official-logo-resmi-white-new.webp") },
+        { name: "agoda", src: require("@/assets/logo/agoda_mainlogo_horizontal_positive_ai_Logo without circles.png") },
+        { name: "tripadvisor", src: require("@/assets/logo/Tripadvisor_Logo_horizontal-lockup_registered_RGB.svg") },
+        { name: "tiket.com", src: require("@/assets/logo/Tiket.com Logo (SVG-1080p) - FileVector69.svg")}
+      ]
+
+      return logos.find(logo => props.name.toLowerCase() === logo.name)?.src
+    })
+
+    return { icon, color, bigLogoSrc }
   },
 })
 </script>
@@ -56,13 +76,25 @@ export default defineComponent({
   <v-btn
     class="mb-2 mb-lg-0 mr-0 mr-lg-2"
     :color="color"
-    outlined
+    :outlined="!bigLogoSrc"
     :href="url"
     target="_blank"
     tile
     :block="!isOnPC"
   >
-    <v-icon left>{{ icon }}</v-icon>
-    {{username.length ? username : name}}
+    <img class="big-btn" v-if="bigLogoSrc" :src="bigLogoSrc" />
+    <fragment v-else>
+      <v-icon left>{{ icon }}</v-icon>
+      {{username.length ? username : name}}
+    </fragment>
   </v-btn>
 </template>
+
+<style lang="scss" scoped>
+  @import '~vuetify/src/components/VBtn/_variables.scss';
+  
+  .big-btn {
+    height: map-get($btn-sizes, "default")+px;
+    object-fit: contain;
+  }
+</style>
